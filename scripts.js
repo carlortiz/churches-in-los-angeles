81a785db1940931c9de444553ba981e4078f3a5d
carlortiz <mr.carlortiz@gmail.com>
2024-04-07 15:14:15 -0700

    add more church data

--- scripts.js
+++ scripts.js
@@ -25,7 +25,7 @@
 
 // This array of objects stores all the data for each individual church.
 let churches = [
-    {
+    { // SAN FERNANDO VALLEY
         name: "Sheperd of the Hills Church - Porter Ranch",
         region: "San Fernando Valley",
         services: [
@@ -87,7 +87,436 @@ let churches = [
         languages: ["English", "Korean"],
         image: "https://scng-dash.digitalfirstmedia.com/wp-content/uploads/2019/11/Northridgeunitedmeth-16x9.jpgnov2019.jpg",
         website: "https://northridgeumc.org/",
-    }
+    },
+    { // CENTRAL LOS ANGELES
+        name: "Our Lady Queen of Angels Catholic Church",
+        region: "Central Los Angeles",
+        services: [
+            {
+                day: "Sunday",
+                time: "8:00 AM",
+            },
+            {
+                day: "Sunday",
+                time: "10:00 AM",
+            },
+            {
+                day: "Sunday",
+                time: "12:00 PM",
+            },
+            {
+                day: "Sunday",
+                time: "1:30 AM",
+            },
+            {
+                day: "Sunday",
+                time: "1:30 AM",
+            },
+            {
+                day: "Monday to Friday",
+                time: "12:00 PM",
+            },
+        ],
+        languages: ["English"],
+        image: "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/15/f6/be/ed/facciata.jpg?w=1200&h=1200&s=1",
+        website: "https://laplacita.mordntedigital.com/",
+    },
+    {
+        name: "New City Church of Los Angeles",
+        region: "Central Los Angeles",
+        services: [
+            {
+                day: "Sunday",
+                time: "10:00 AM",
+            },
+        ],
+        languages: ["English"],
+        image: "https://newcitychurchla.com/wp-content/uploads/2012/01/latc2.jpg",
+        website: "https://newcitychurchla.com/about/",
+    },
+    { 
+        name: "Cathedral of Our Lady of the Angels",
+        region: "Central Los Angeles",
+        services: [
+            {
+                day: "Sunday",
+                time: "10:00 AM",
+            },
+            {
+                day: "Sunday",
+                time: "10:00 AM",
+            },
+            {
+                day: "Sunday",
+                time: "12:30 PM",
+            },
+            {
+                day: "Monday to Friday",
+                time: "7:00 AM",
+            },
+            {
+                day: "Monday to Friday",
+                time: "12:10 PM",
+            },
+        ],
+        languages: ["English", "Spanish"],
+        image: "https://rafaelmoneo.com/wp/wp-content/uploads/2016/10/0620-09Fweb.jpg",
+        website: "https://olacathedral.org/mass-schedule",
+    },
+    { // WESTSIDE
+        name: "The Westside Church",
+        region: "Westside",
+        services: [
+            {
+                day: "Sunday",
+                time: "10:30 AM",
+            },
+            {
+                day: "Wednesday",
+                time: "7:00 PM",
+            },
+        ],
+        languages: ["English"],
+        image: "https://dq5pwpg1q8ru0.cloudfront.net/2022/04/19/14/56/18/db58a03d-2bfd-4053-860b-b9ff7586f60a/Screen%20Shot%202022-04-12%20at%204.51.40%20PM.png",
+        website: "https://thewestsidechurch.com/",
+    },
+    { 
+        name: "Cornerstone Church of West Los Angeles",
+        region: "Westside",
+        services: [
+            {
+                day: "Sunday (English)",
+                time: "9:00 AM",
+            },
+            {
+                day: "Sunday (Spanish)",
+                time: "9:00 PM",
+            },
+            {
+                day: "Sunday (English)",
+                time: "11:00 AM",
+            },
+            {
+                day: "Sunday (Farsi)",
+                time: "11:00 PM",
+            },
+        ],
+        languages: ["English", "Spanish", "Farsi"],
+        image: "https://storage2.snappages.site/KVJZNX/assets/images/4591149_4368x2912_500.jpeg",
+        website: "https://cornerstonewla.org/",
+    },
+    { 
+        name: "Vintage Church L.A.",
+        region: "Westside",
+        services: [
+            {
+                day: "Sunday",
+                time: "8:15 AM",
+            },
+            {
+                day: "Sunday",
+                time: "10:00 PM",
+            },
+            {
+                day: "Sunday",
+                time: "11:45 AM",
+            },
+        ],
+        languages: ["English"],
+        image: "https://images.squarespace-cdn.com/content/v1/5edd01c858851067a69b290e/a7c2a343-71d4-4051-af6b-c57489f200e7/20230827_105411_0003.jpg",
+        website: "https://www.vintagechurchla.com/new",
+    }, 
+    { // SAN GABRIEL VALLEY
+        name: "Saint Anthony Catholic Church",
+        region: "San Gabriel Valley",
+        services: [
+            {
+                day: "Sunday",
+                time: "6:30 AM",
+            },
+            {
+                day: "Sunday",
+                time: "8:30 AM",
+            },
+            {
+                day: "Sunday",
+                time: "10:15 AM",
+            },
+            {
+                day: "Sunday",
+                time: "1:00 PM",
+            },
+            {
+                day: "Sunday",
+                time: "5:00 PM",
+            },
+            {
+                day: "Monday to Saturday",
+                time: "8:00 AM",
+            },
+            {
+                day: "Monday to Saturday",
+                time: "9:00 AM",
+            },
+            {
+                day: "Sunday",
+                time: "7:00 PM",
+            },
+        ],
+        languages: ["English", "Spanish"],
+        image: "",
+        website: "https://stanthonysg.org/",
+    },
+    {
+        name: "San Gabriel Community Church",
+        region: "San Gabriel Valley",
+        services: [
+            {
+                day: "Sunday",
+                time: "9:00 AM",
+            },
+        ],
+        languages: ["English"],
+        image: "",
+        website: "https://sangabrielcommunity.org/",
+    },
+    {
+        name: "First Evangelical Church of San Gabriel Valley",
+        region: "San Gabriel Valley",
+        services: [
+            {
+                day: "Sunday",
+                time: "9:30 AM",
+            },
+        ],
+        languages: ["English"],
+        image: "",
+        website: "https://www.sgvconnections.com/sunday-service",
+    }, 
+    { // SOUTH BAY
+        name: "South Bay Community Church",
+        region: "South Bay",
+        services: [
+            {
+                day: "Sunday",
+                time: "9:00 AM",
+            },
+            {
+                day: "Sunday",
+                time: "11:00 AM",
+            },
+            {
+                day: "Saturday",
+                time: "5:00 PM",
+            },
+        ],
+        languages: ["English"],
+        image: "",
+        website: "https://southbaycommunitychurch.com/",
+    },
+    {
+        name: "Journey South Bay Church",
+        region: "South Bay",
+        services: [
+            {
+                day: "Sunday",
+                time: "9:00 AM",
+            },
+            {
+                day: "Sunday",
+                time: "10:45 AM",
+            },
+        ],
+        languages: ["English"],
+        image: "",
+        website: "https://www.journeysouthbay.org/",
+    },
+    {
+        name: "South Bay Church of God",
+        region: "South Bay",
+        services: [
+            {
+                day: "Sunday",
+                time: "11:00 AM",
+            },
+        ],
+        languages: ["English"],
+        image: "",
+        website: "https://www.sbcog.org/",
+    }, 
+    { // ANTELOPE VALLEY
+        name: "Edwards Air Force Base Chapels 1 & 2",
+        region: "Antelope Valley",
+        services: [
+            {
+                day: "Sunday (Chapel 2)",
+                time: "9:00 AM",
+            },
+            {
+                day: "Sunday (Chapel 1)",
+                time: "10:00 AM",
+            },
+            {
+                day: "Sunday (Chapel 2)",
+                time: "4:30 PM",
+            },
+            {
+                day: "Monday (Chapel 2)",
+                time: "11:30 AM",
+            },
+            {
+                day: "Tuesday (Chapel 2)",
+                time: "11:30 AM",
+            },
+            {
+                day: "Thursday (Chapel 2)",
+                time: "11:30 AM",
+            },
+        ],
+        languages: ["English"],
+        image: "",
+        website: "https://www.edwards.af.mil/About/Fact-Sheets/Display/Article/393896/412th-test-wing-chaplain-service/",
+    },
+    {
+        name: "Antelope Valley Church",
+        region: "Antelope Valley",
+        services: [
+            {
+                day: "Sunday",
+                time: "10:45 AM",
+            },
+            {
+                day: "Sunday",
+                time: "7:00 PM",
+            },
+        ],
+        languages: ["English"],
+        image: "",
+        website: "https://www.avchurch.org/",
+    },
+    {
+        name: "Central Christian Church",
+        region: "Antelope Valley",
+        services: [
+            {
+                day: "Sunday",
+                time: "9:00 AM",
+            },
+            {
+                day: "Sunday",
+                time: "10:45 AM",
+            },
+        ],
+        languages: ["English"],
+        image: "",
+        website: "https://www.centralchristian.org/",
+    },
+    { // SOUTHEAST LOS ANGELES
+        name: "Revive Church",
+        region: "Southeast Los Angeles",
+        services: [
+            {
+                day: "Sunday",
+                time: "11:00 AM",
+            },
+            {
+                day: "Sunday",
+                time: "7:30 PM",
+            },
+        ],
+        languages: ["English", "Spanish"],
+        image: "",
+        website: "https://www.revivelachurch.org/",
+    }, 
+    {
+        name: "City Bible Church",
+        region: "Southeast Los Angeles",
+        services: [
+            {
+                day: "Sunday",
+                time: "9:30 AM",
+            },
+            {
+                day: "Sunday",
+                time: "3:00 PM",
+            },
+        ],
+        languages: ["English"],
+        image: "",
+        website: "https://www.citybible.church/",
+    }, 
+    {
+        name: "Firehouse Church",
+        region: "Southeast Los Angeles",
+        services: [
+            {
+                day: "Sunday",
+                time: "9:30 AM",
+            },
+            {
+                day: "Wednesday",
+                time: "6:30 AM",
+            },
+        ],
+        languages: ["English"],
+        image: "",
+        website: "https://firehousechurch.info/",
+    }, 
+    { // SANTA CLARITA
+        name: "Santa Clarita Christian Fellowship Church",
+        region: "Santa Clarita",
+        services: [
+            {
+                day: "Sunday",
+                time: "8:00 AM",
+            },
+            {
+                day: "Sunday",
+                time: "10:00 AM",
+            },
+        ],
+        languages: ["English"],
+        image: "",
+        website: "https://mysccf.com/",
+    },
+    {
+        name: "Grace Baptist Church",
+        region: "Santa Clarita",
+        services: [
+            {
+                day: "Sunday",
+                time: "8:45 AM",
+            },
+            {
+                day: "Sunday",
+                time: "10:45 AM",
+            },
+        ],
+        languages: ["English"],
+        image: "",
+        website: "https://www.gracebaptist.org/",
+    },
+    {
+        name: "Elevate Church",
+        region: "Santa Clarita",
+        services: [
+            {
+                day: "Sunday",
+                time: "8:30 AM",
+            },
+            {
+                day: "Sunday",
+                time: "10:00 AM",
+            },
+            {
+                day: "Sunday",
+                time: "11:30 AM",
+            },
+        ],
+        languages: ["English"],
+        image: "",
+        website: "https://www.ielevatechurch.com/",
+    },
 ];
 
 // This function adds cards the page to display the data in the array
