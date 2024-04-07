419d532881f08b71715823e7e828d5b511032f75
carlortiz <mr.carlortiz@gmail.com>
2024-04-07 10:43:37 -0700

    initial commit

--- scripts.js
+++ scripts.js
@@ -23,20 +23,69 @@
  * 
  */
 
-
-const FRESH_PRINCE_URL = "https://upload.wikimedia.org/wikipedia/en/3/33/Fresh_Prince_S1_DVD.jpg";
-const CURB_POSTER_URL = "https://m.media-amazon.com/images/M/MV5BZDY1ZGM4OGItMWMyNS00MDAyLWE2Y2MtZTFhMTU0MGI5ZDFlXkEyXkFqcGdeQXVyMDc5ODIzMw@@._V1_FMjpg_UX1000_.jpg";
-const EAST_LOS_HIGH_POSTER_URL = "https://static.wikia.nocookie.net/hulu/images/6/64/East_Los_High.jpg";
-
-// This is an array of strings (TV show titles)
-let titles = [
-    "Fresh Prince of Bel Air",
-    "Curb Your Enthusiasm",
-    "East Los High"
+// This array of objects stores all the data for each individual church.
+let churches = [
+    {
+        name: "Sheperd of the Hills Church - Porter Ranch",
+        region: "San Fernando Valley",
+        services: [
+            {
+                day: "Saturday",
+                time: "6:00 PM",
+            },
+            {
+                day: "Sunday",
+                time: "9:00 AM",
+            },
+            {
+                day: "Sunday",
+                time: "11:00 AM",
+            },
+            {
+                day: "Thursday",
+                time: "7:00 PM",
+            },
+        ],
+        languages: ["English"],
+        image: "https://irwinseating-images.s3.amazonaws.com/Installations/Shepherd-Church/Shepherd-Church-0590.jpg",
+    },
+    {
+        name: "Sheperd of the Hills Church - Woodland Hills",
+        region: "San Fernando Valley",
+        services: [
+            {
+                day: "Sunday",
+                time: "8:30 AM",
+            },
+            {
+                day: "Sunday",
+                time: "10:00 AM",
+            },
+            {
+                day: "Sunday",
+                time: "11:30 AM",
+            },
+        ],
+        languages: ["English"],
+        image: "https://i0.wp.com/premiumsignsolutions.com/wp-content/uploads/2023/03/Monument-sign1_Shepherd-Chruch_Woodland-Hills3.jpg?resize=300%2C200&ssl=1",
+    },
+    {
+        name: "Northridge United Methodist Church",
+        region: "San Fernando Valley",
+        services: [
+            {
+                day: "Sunday",
+                time: "10:00 AM",
+            },
+            {
+                day: "Sunday",
+                time: "11:30 AM",
+            },
+        ],
+        languages: ["English", "Korean"],
+        image: "https://scng-dash.digitalfirstmedia.com/wp-content/uploads/2019/11/Northridgeunitedmeth-16x9.jpgnov2019.jpg",
+    }
 ];
-// Your final submission should have much more data than this, and 
-// you should use more than just an array of strings to store it all.
-
 
 // This function adds cards the page to display the data in the array
 function showCards() {
@@ -44,22 +93,11 @@ function showCards() {
     cardContainer.innerHTML = "";
     const templateCard = document.querySelector(".card");
     
-    for (let i = 0; i < titles.length; i++) {
-        let title = titles[i];
-
-        // This part of the code doesn't scale very well! After you add your
-        // own data, you'll need to do something totally different here.
-        let imageURL = "";
-        if (i == 0) {
-            imageURL = FRESH_PRINCE_URL;
-        } else if (i == 1) {
-            imageURL = CURB_POSTER_URL;
-        } else if (i == 2) {
-            imageURL = EAST_LOS_HIGH_POSTER_URL;
-        }
+    for (let i = 0; i < churches.length; i++) {
+        let church = churches[i];
 
         const nextCard = templateCard.cloneNode(true); // Copy the template card
-        editCardContent(nextCard, title, imageURL); // Edit title and image
+        editCardContent(nextCard, church.name, church.image); // Edit title and image
         cardContainer.appendChild(nextCard); // Add new card to the container
     }
 }
